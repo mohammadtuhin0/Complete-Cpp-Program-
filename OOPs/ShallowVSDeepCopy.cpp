3fe498fa482d06906c1bdfa1205b2010aa595221
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-11-16 13:07:28 +0600

    Basic Shallow & Deep Constructor

---
+++ OOPs/ShallowVSDeepCopy.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    double* cgpaPtr;
+
+    Student(string name, double cgpa) {
+        this-> name = name;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
+    }
+
+    Student(Student &obj) {
+        this-> name = obj.name;
+        cgpaPtr = new double;
+        *cgpaPtr = *obj.cgpaPtr;
+    }
+    
+    void getInfo() {
+        cout<< "Name : "<< name <<endl;
+        cout<< "CGPA : "<< *cgpaPtr <<endl;
+    }
+};
+
+int main() {
+    Student s1("rahul kumar", 8.9);
+    Student s2(s1);
+
+    s1.getInfo();
+    *(s2.cgpaPtr) = 9.2;
+    s1.getInfo();
+
+    s2.name = "Neha";
+    s2.getInfo();
+    return 0;
+}
