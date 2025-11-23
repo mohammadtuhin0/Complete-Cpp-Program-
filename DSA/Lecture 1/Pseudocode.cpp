c92a3a3b85acdba4045c8a0d375d0efb437b6e96
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-11-23 16:37:55 +0600

    basic pseudocode

---
+++ DSA/Lecture 1/Pseudocode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y, sum;
+    cin>>x;
+    cin>>y;
+    sum = x + y;
+    cout<<sum;
+}
+
+// Pseducode
+// 1. Input a & b
+// 2. sum = a & b
+// 3. Print sum
+// 4. Exit
